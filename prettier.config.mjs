d8ccf78f7ecd09e8773e0f10d39c907a3ef7117b
Adam Wojtycki <adamwojtycki@gmail.com>
2024-02-16 10:56:27 +0100

    chore: add prettier, clean template stuff

---
+++ prettier.config.mjs
@@ -0,0 +1,21 @@
+/** @typedef  {import("prettier").Config} PrettierConfig */
+/** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
+/** @typedef  {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
+
+/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
+const config = {
+  plugins: ["@ianvs/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
+  tailwindConfig: "./tailwind.config.ts",
+  importOrder: [
+    "^(react/(.*)$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^@/",
+    "^[../]",
+    "^[./]",
+  ],
+  printWidth: 100,
+};
+
+export default config;
---
+++ src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <main className="">1</main>;
+}
