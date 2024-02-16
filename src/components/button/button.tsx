cf6608949eadf14a200efafecbe10b0c97d65005
Adam Wojtycki <adamwojtycki@gmail.com>
2024-02-16 11:31:06 +0100

    feat: add button component

---
+++ src/components/button/button.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+import * as React from "react";
+
+const buttonVariants = cva(
+  "inline-flex select-none items-center justify-center rounded-[5px] whitespace-nowrap font-semibold transition-colors aria-disabled:pointer-events-none aria-disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-midnight-purple text-white hover:[@media(pointer:fine)]:bg-midnight-purple-hover active:bg-midnight-purple-active",
+        outline:
+          "text-purple-gray/0.2 border border-purple-gray bg-light-purple hover:[@media(pointer:fine)]:bg-light-purple-hover active:bg-light-purple-active",
+      },
+      size: {
+        default: "h-12 px-6 py-4 text-base w-[238px] h-[52px]",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  },
+);
+
+function disabledFromProps(props: {
+  disabled?: boolean;
+  "aria-disabled"?: boolean | "false" | "true";
+}) {
+  return props.disabled || props["aria-disabled"] === true || props["aria-disabled"] === "true";
+}
+
+function useDisableEvent(onEvent?: React.EventHandler<React.SyntheticEvent>, disabled?: boolean) {
+  return (event: React.SyntheticEvent) => {
+    onEvent?.(event);
+
+    if (event.defaultPrevented) {
+      return;
+    }
+
+    if (disabled) {
+      event.stopPropagation();
+      event.preventDefault();
+    }
+  };
+}
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean;
+    allowAccessiblyDisabled?: boolean;
+  };
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, asChild = false, allowAccessiblyDisabled, variant, size, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
+
+    const disabled = disabledFromProps(props);
+    const fullyDisabled = allowAccessiblyDisabled && disabled;
+    const onKeyUpCapture = useDisableEvent(props.onKeyUp, disabled);
+    const onClickCapture = useDisableEvent(props.onClickCapture, disabled);
+    const onMouseDownCapture = useDisableEvent(props.onMouseDownCapture, disabled);
+
+    return (
+      <Comp
+        className={buttonVariants({ variant, size, className })}
+        ref={ref}
+        {...props}
+        onClickCapture={onClickCapture}
+        onMouseDownCapture={onMouseDownCapture}
+        onKeyUp={onKeyUpCapture}
+        aria-disabled={disabled}
+        disabled={fullyDisabled}
+      />
+    );
+  },
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants, type ButtonProps };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,22 +3,26 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    colors: {
-      salmon: "hsl(var(--salmon))",
-      "blue-gray": {
-        900: "hsl(var(--blue-gray-900))",
-      },
-      "purple-gray": "hsl(var(--purple-gray))",
-      "midnight-gray": "hsl(var(--midnight-gray))",
-
-      "midnight-purple": {
-        DEFAULT: "hsl(var(--midnight-purple))",
-        hover: "hsl(var(--midnight-purple-hover))",
-        active: "hsl(var(--midnight-purple-active))",
-      },
-    },
     extend: {
       colors: {
+        salmon: "hsl(var(--salmon))",
+        "blue-gray": {
+          900: "hsl(var(--blue-gray-900))",
+        },
+        "purple-gray": "hsl(var(--purple-gray))",
+        "midnight-gray": "hsl(var(--midnight-gray))",
+
+        "light-purple": {
+          DEFAULT: "hsla(var(--light-purple))",
+          hover: "hsla(var(--light-purple-hover))",
+          active: "hsla(var(--light-purple-active))",
+        },
+
+        "midnight-purple": {
+          DEFAULT: "hsl(var(--midnight-purple))",
+          hover: "hsl(var(--midnight-purple-hover))",
+          active: "hsl(var(--midnight-purple-active))",
+        },
         background: "hsl(var(--background))",
       },
     },
